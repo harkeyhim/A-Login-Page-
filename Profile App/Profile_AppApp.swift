08675aa75f29ebc96f0dcac58f204dd7c5b2fbb8
Iam_Hey-k <60710389+harkeyhim@users.noreply.github.com>
2023-09-19 12:22:47 +0100

    Initial Commit

---
+++ Profile App/Profile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_AppApp.swift
+//  Profile App
+//
+//  Created by Yusuf Akeem on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Profile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
